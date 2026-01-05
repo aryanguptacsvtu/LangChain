6af04d511415c8962c5f971b1149ab7db6d60fa6
Aryan Gupta <148983503+aryanguptacsvtu@users.noreply.github.com>
2026-01-05 09:04:50 +0530

    parallel_chain

---
+++ 7_Chains/parallel_chain.py
@@ -0,0 +1,47 @@
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+from langchain.schema.runnable import RunnableParallel
+
+load_dotenv()
+
+model1 = ChatGroq(model="llama-3.1-8b-instant", temperature=0.5)
+model2 = ChatGroq(model="gemma2-9b-it", temperature=0.5)
+parser = StrOutputParser()
+
+prompt1 = PromptTemplate(template="Generate short and simple notes from the following text \n{text}",
+                         input_variables=["text"])
+
+prompt2 = PromptTemplate(template="Generate 5 short question answers for the following text \n{text}",
+                         input_variables=["text"])
+
+prompt3 = PromptTemplate(template="Merge the provided notes and quiz into a single document\nNotes: {notes}\nQuiz: {quiz}",
+                         input_variables=["notes", "quiz"])
+
+
+parallel_chain = RunnableParallel( {
+                                    'notes': prompt1 | model1 | parser,
+                                    'quiz': prompt2 | model2 | parser
+                                   })
+
+merge_chain = prompt3 | model1 | parser
+
+final_chain = parallel_chain | merge_chain
+
+text = """Support vector machines (SVMs) are a set of supervised learning methods used for classification, regression and outliers detection.
+The advantages of support vector machines are:
+Effective in high dimensional spaces.
+Still effective in cases where number of dimensions is greater than the number of samples.
+Uses a subset of training points in the decision function (called support vectors), so it is also memory efficient.
+Versatile: different Kernel functions can be specified for the decision function. Common kernels are provided, but it is also possible to specify custom kernels.
+The disadvantages of support vector machines include:
+If the number of features is much greater than the number of samples, avoid over-fitting in choosing Kernel functions and regularization term is crucial.
+SVMs do not directly provide probability estimates, these are calculated using an expensive five-fold cross-validation (see Scores and probabilities, below).
+The support vector machines in scikit-learn support both dense (numpy.ndarray and convertible to that by numpy.asarray) and sparse (any scipy.sparse) sample vectors as input. However, to use an SVM to make predictions for sparse data, it must have been fit on such data. For optimal performance, use C-ordered numpy.ndarray (dense) or scipy.sparse.csr_matrix (sparse) with dtype=float64.
+"""
+
+result = final_chain.invoke({"text": text})
+print(result)
+
+final_chain.get_graph().print_ascii()
